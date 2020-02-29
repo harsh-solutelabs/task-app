b17deb4b69b7e7ea054bf402e811ae018d87572b
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-29 18:37:34 +0530

    playground aysc await

--- playground/promise-channing.js
+++ playground/promise-channing.js
@@ -1,14 +1,28 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("5e590f3a0300271f81d48573", { name: "Change" })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ name: "Browser" });
-  })
+// User.findByIdAndUpdate("5e590f3a0300271f81d48573", { name: "Change" })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ name: "Browser" });
+//   })
+//   .then(count => {
+//     console.log(count);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const updateNameGetCount = async (id, name) => {
+  const user = await User.findByIdAndUpdate(id, { name });
+  const count = await User.countDocuments({ name });
+  return count;
+};
+
+updateNameGetCount("5e590f3a0300271f81d48573", "harshmeht")
   .then(count => {
     console.log(count);
   })
   .catch(e => {
-    console.log(e);
+    console.log("ddddddddd", e);
   });
--- playground/promise-channing2.js
+++ playground/promise-channing2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5e591367ea1cf9224797f788")
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(incomplete => {
-    console.log(incomplete);
+// Task.findByIdAndDelete("5e591367ea1cf9224797f788")
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(incomplete => {
+//     console.log(incomplete);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndIncompleted = async (id, completed) => {
+  const user = await Task.findByIdAndDelete(id);
+  const userCompleted = await Task.countDocuments({ completed });
+  return userCompleted;
+};
+
+deleteTaskAndIncompleted("5e59145bc4b1b323015b9a10", "true")
+  .then(complete => {
+    console.log(complete);
   })
   .catch(e => {
     console.log(e);
